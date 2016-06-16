32d57655b3c1f6357727b63e3e362492ed3129e4
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-06-16 10:28:02 -0300

    Add tests for lookfile of Lookfile module

--- spec/lookfile_spec.rb
+++ spec/lookfile_spec.rb
@@ -5,7 +5,22 @@ describe Lookfile do
     expect(Lookfile::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'Initialize Lookfile directory' do
+    it 'get lookfile with default base dir' do
+      lookfile_dir = Lookfile.lookfile_dir
+      default_base_dir = File.expand_path(Lookfile::BASE_DIR)
+
+      expect(lookfile_dir).to include(default_base_dir)
+      expect(lookfile_dir).to include(Lookfile::LOOKFILE_DIR)
+    end
+
+    it 'get lookfile with setted base dir' do
+      base_dir = File.expand_path('~/Documents')
+      lookfile_dir = Lookfile.lookfile_dir(base_dir)
+
+      expect(lookfile_dir).to include(base_dir)
+      expect(lookfile_dir).to include(Lookfile::LOOKFILE_DIR)
+    end
   end
+
 end
