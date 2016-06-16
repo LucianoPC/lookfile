3636b51e4b631dafd0779d2ebb32316383843a1f
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-06-16 10:27:43 -0300

    Add initialize for Lookfile

--- lib/lookfile.rb
+++ lib/lookfile.rb
@@ -2,5 +2,20 @@ require 'lookfile/version'
 
 # main module of lookfile gem
 module Lookfile
-  # Your code goes here...
+  module_function
+
+  BASE_DIR = '~/'
+  LOOKFILE_DIR = '.lookfile/'.freeze
+
+  def initialize
+    return false if File.directory?(LOOKFILE_DIR)
+    Dir.mkdir(LOOKFILE_DIR)
+    true
+  end
+
+  def lookfile_dir(base_dir = BASE_DIR)
+    base_dir = File.expand_path(base_dir)
+    base_dir += '/' if base_dir[-1] != '/'
+    base_dir + LOOKFILE_DIR
+  end
 end
