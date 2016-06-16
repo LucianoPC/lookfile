ddc3c7f3e3cd250cdf2c778ae69bb285ac86d93b
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-06-16 18:28:15 -0300

    Add function to add files into lookfile

--- lib/lookfile.rb
+++ lib/lookfile.rb
@@ -1,23 +1,42 @@
 require 'lookfile/version'
+require 'fileutils'
 
 # main module of lookfile gem
 module Lookfile
   module_function
 
   BASE_DIR = '~/'.freeze
-  LOOKFILE_DIR = '.lookfile/'.freeze
+  LOOKFILE_DIR = '.lookfile'.freeze
 
   def initialize(base_dir = BASE_DIR)
-    dir = lookfile_dir(base_dir)
-    puts "initialize: #{File.directory?(dir)}"
-    return nil if File.directory?(dir)
-    Dir.mkdir(dir)
-    dir
+    lookfile_dir = load_lookfile_dir(base_dir)
+    return nil if File.directory?(lookfile_dir)
+    Dir.mkdir(lookfile_dir)
+    lookfile_dir
   end
 
-  def lookfile_dir(base_dir = BASE_DIR)
+  def load_lookfile_dir(base_dir = BASE_DIR)
     base_dir = File.expand_path(base_dir)
     base_dir += '/' if base_dir[-1] != '/'
     base_dir + LOOKFILE_DIR
+  end
+
+  def add_files(files_path, base_dir = BASE_DIR)
+    files_path = [files_path] unless files_path.is_a?(Array)
+    files_path = files_path.map { |file_path| File.expand_path(file_path) }
+    lookfile_dir = load_lookfile_dir(base_dir)
+    added_files = []
+    error_files = []
+    files_path.each do |file_path|
+      folder_path = lookfile_dir + file_path.scan(/(.+)\//).flatten.first
+      FileUtils.mkpath(folder_path)
+      begin
+        FileUtils.cp(file_path, folder_path)
+        added_files << file_path
+      rescue
+        error_files << file_path
+      end
+    end
+    [added_files, error_files]
   end
 end
--- spec/lookfile_spec.rb
+++ spec/lookfile_spec.rb
@@ -2,14 +2,17 @@ require 'spec_helper'
 require 'fileutils'
 
 BASE_DIR = File.expand_path('~/.test_lookfile')
+TEST_FILE = File.expand_path('~/.test_file')
 
 describe Lookfile do
   before do
     Dir.mkdir(BASE_DIR)
+    FileUtils.touch(TEST_FILE)
   end
 
   after do
     FileUtils.rm_rf(BASE_DIR)
+    FileUtils.rm_rf(TEST_FILE)
   end
 
   it 'has a version number' do
@@ -18,7 +21,7 @@ describe Lookfile do
 
   describe 'Initialize Lookfile directory' do
     it 'get lookfile with default base dir' do
-      lookfile_dir = Lookfile.lookfile_dir
+      lookfile_dir = Lookfile.load_lookfile_dir
       default_base_dir = File.expand_path(Lookfile::BASE_DIR)
 
       expect(lookfile_dir).to include(default_base_dir)
@@ -27,7 +30,7 @@ describe Lookfile do
 
     it 'get lookfile with setted base dir' do
       base_dir = File.expand_path(BASE_DIR)
-      lookfile_dir = Lookfile.lookfile_dir(base_dir)
+      lookfile_dir = Lookfile.load_lookfile_dir(base_dir)
 
       expect(lookfile_dir).to include(base_dir)
       expect(lookfile_dir).to include(Lookfile::LOOKFILE_DIR)
@@ -37,6 +40,22 @@ describe Lookfile do
       lookfile_dir = Lookfile.initialize(BASE_DIR)
 
       expect(lookfile_dir).to eq "#{BASE_DIR}/#{Lookfile::LOOKFILE_DIR}"
+    end
+  end
+
+  describe 'Add files to lookfile folder' do
+    it 'can add an existing file' do
+      added_files, _ = Lookfile.add_files(TEST_FILE)
+
+      expect(added_files).to include(TEST_FILE)
+    end
+
+    it 'can not add an non existent file' do
+      non_existent_file_name = '~/.lookfile_non_existent_file'
+      non_existent_file_name = File.expand_path(non_existent_file_name)
+      _, error_files = Lookfile.add_files(non_existent_file_name)
+
+      expect(error_files).to include(non_existent_file_name)
     end
   end
 end
