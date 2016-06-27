a8a9b3a509a1322e8fe2483a34dd170065e7bd76
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-06-27 18:23:52 -0300

    Add coveralls

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in lookfile.gemspec
 gemspec
+gem 'coveralls', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'lookfile'
+require 'coveralls'
+Coveralls.wear!
 
 BASE_DIR = File.expand_path('~/.test_lookfile')
 GIT_DIR = File.expand_path('~/.test_lookfile_git')
